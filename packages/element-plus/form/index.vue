f4af428cc8f699969ae6a68c0cf73491b31025f9
赵亦凡 <1784848264@qq.com>
2025-03-03 14:10:46 +0800

    tvue_3.x

--- packages/element-plus/form/index.vue
+++ packages/element-plus/form/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="[b(),{'tvue-detail':isDetail}]"
-       :style="{width:setPx(tableOption.formWidth,'100%')}">
+       :style="{width:setPx(tableOption.formWidth,'100%'),maxHeight:(tableOption.formMaxHeight,'650px'),overflowY:'scroll'}">
     <el-form ref="form"
              :status-icon="tableOption.statusIcon"
              @submit.prevent
